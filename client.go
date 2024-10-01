346b630a2da76feb687b7b844e2b91672673afeb
0xN0x <n0x@n0x.cc>
2024-10-02 01:44:49 +0200

    refactor: change the way to make request

--- client.go
+++ client.go
@@ -1,9 +1,7 @@
 package artifactsmmo
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"sync"
 
@@ -18,7 +16,7 @@ type ArtifactsMMO struct {
 	mu       sync.Mutex
 	token    string
 	username string
-	client   http.Client
+	client   *http.Client
 }
 
 func NewClient(token string, username string) *ArtifactsMMO {
@@ -26,88 +24,41 @@ func NewClient(token string, username string) *ArtifactsMMO {
 		mu:       sync.Mutex{},
 		token:    token,
 		username: username,
-		client:   http.Client{},
+		client:   &http.Client{},
 	}
 }
 
-func (c *ArtifactsMMO) makeRequest(method string, action string) (*http.Response, error) {
-	url := fmt.Sprintf("%s/%s", apiUrl, action)
-
-	req, _ := http.NewRequest(method, url, nil)
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.token))
-
-	if method == "POST" {
-		req.Header.Add("Content-Type", "application/json")
+func NewClientWithCustomHttpClient(token string, username string, client *http.Client) *ArtifactsMMO {
+	return &ArtifactsMMO{
+		mu:       sync.Mutex{},
+		token:    token,
+		username: username,
+		client:   client,
 	}
-
-	res, err := c.client.Do(req)
-
-	switch res.StatusCode {
-	case 452:
-		return nil, fmt.Errorf("failed to parse token: token is missing or empty")
-	case 486:
-		return nil, fmt.Errorf("an action is already in progress by your character")
-	case 497:
-		return nil, fmt.Errorf("character inventory is full")
-	case 498:
-		return nil, fmt.Errorf("character not found")
-	case 499:
-		return nil, fmt.Errorf("character in cooldown")
-	}
-
-	return res, err
 }
 
 func (c *ArtifactsMMO) Fight() (*models.CharacterFight, error) {
-	res, err := c.makeRequest("POST", fmt.Sprintf("my/%s/action/fight", c.username))
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer func() {
-		err := res.Body.Close()
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	if res.StatusCode == 598 {
-		return nil, fmt.Errorf("monster not found on this map")
-	}
-
-	body, _ := io.ReadAll(res.Body)
-
 	var fight struct {
 		Data models.CharacterFight `json:"data"`
 	}
 
-	json.Unmarshal(body, &fight)
+	_, err := NewRequest(c, &fight, "POST", fmt.Sprintf("%s/my/%s/action/fight", apiUrl, c.username), nil).Run()
+	if err != nil {
+		return nil, err
+	}
 
 	return &fight.Data, nil
 }
 
 func (c *ArtifactsMMO) GetCharacterInfo() (*models.Character, error) {
-	res, err := c.makeRequest("GET", fmt.Sprintf("characters/%s", c.username))
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer func() {
-		err := res.Body.Close()
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	body, _ := io.ReadAll(res.Body)
-
 	var character struct {
 		Data models.Character `json:"data"`
 	}
-	json.Unmarshal(body, &character)
+
+	_, err := NewRequest(c, &character, "GET", fmt.Sprintf("%s/characters/%s", apiUrl, c.username), nil).Run()
+	if err != nil {
+		return nil, err
+	}
 
 	return &character.Data, nil
 }
