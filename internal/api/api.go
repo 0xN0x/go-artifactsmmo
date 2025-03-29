a28a8d78f7f6812610582e3745ed7e51a754f5d6
pyssouza <pabloyuriss@hotmail.com>
2025-03-29 16:39:05 -0300

    feat: add unit tests for api

--- internal/api/api.go
+++ internal/api/api.go
@@ -40,9 +40,17 @@ func (hc *HTTPRequest) Set() *HTTPRequest {
 	return hc
 }
 
+func (hc *HTTPRequest) GetRequest() *http.Request {
+	return hc.req
+}
+
 func (hc *HTTPRequest) SetResultStruct(customStruct any) *HTTPRequest {
 	hc.customStruct = customStruct
 	return hc
+}
+
+func (hc *HTTPRequest) GetCustomStruct() any {
+	return hc.customStruct
 }
 
 func (hc *HTTPRequest) SetMethod(method string) *HTTPRequest {
